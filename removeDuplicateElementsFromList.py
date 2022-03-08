a9585d6c473069b45fc13dc09482faed72623cdc
rishabparekh14 <rishabparekh14@gmail.com>
2022-03-07 23:02:27 -0800

    Removing dupliate elements from the list using the same array space

---
+++ removeDuplicateElementsFromList.py
@@ -0,0 +1,36 @@
+def removeDuplicates(nums):
+
+
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i+=1
+            nums[i] = nums[j]
+    print(nums)
+    return i+1
+        
+        
+        ## Using a new array approach
+        
+        # uniqueList = []
+        # count = 0
+        # for x in nums:
+        #     if x not in uniqueList:
+        #         uniqueList.append(x)
+                
+                
+        # for i in range(0, len(uniqueList)-1):
+        #     count = 0
+        #     for rpi in range(0, len(nums)-1):
+        #         if uniqueList[i] == nums[rpi]:
+        #             count += 1
+        #             if count > 1:
+        #                 uniqueList.append(nums[rpi])
+        # nums = uniqueList[:]
+        # print("Sorted elements by pushing duplicates to end:",nums)
+        # return len(nums)
+
+
+numbers = [1,1,2]
+result = removeDuplicates(numbers)
+print(result)
