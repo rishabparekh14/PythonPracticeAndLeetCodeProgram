f2d1de79ab2b38ff790a3ef5bf1a515d4470052c
rishabparekh14 <rishabparekh14@gmail.com>
2022-04-05 11:37:55 -0700

    Deep copy of linkedlist

---
+++ deepCopyLinkedList.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldList = head
+        newList = copy.deepcopy(oldList)
+        return newList
+
+            
+            
+
+        
