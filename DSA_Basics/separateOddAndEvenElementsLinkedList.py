cc04df6437c7fae4d572760e767f29d9dbafbc17
Rishab Parekh <rishabparekh14@gmail.com>
2022-08-09 23:39:47 -0700

    Alternate solution for separating the odd and even list

--- DSA_Basics/separateOddAndEvenElementsLinkedList.py
+++ DSA_Basics/separateOddAndEvenElementsLinkedList.py
@@ -37,3 +37,28 @@ class Solution:
             
         
         return head              
+
+
+## Alternate and easy approach
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd =head
+        even = head
+        if head == None:
+            return None
+        even = head.next
+        evenHead = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even= even.next
+        odd.next = evenHead
+        return head
+            
+            
